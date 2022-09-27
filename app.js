a665e05cecee8e3b3d9da49598ad33773770b68c
David Genesis Pedeglorio <pedegloriodavidgenesis@gmail.com>
2022-09-27 21:35:04 +0800

    Fundamental quiz logic

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 const submitBtn = document.querySelector('ion-button')
+const answerOne = document.getElementById('answer-1')
+const answerTwo = document.getElementById('answer-2')
+const answerThree = document.getElementById('answer-3')
+const answerFour = document.getElementById('answer-4')
+const answerFive = document.getElementById('answer-5')
+
+async function presentAlert() {
+    const alert = document.createElement('ion-alert');
+    alert.header = 'Alert';
+    alert.subHeader = 'Important message';
+    alert.message = 'Make sure to answer all of the questions!';
+    alert.buttons = ['OK'];
+
+    document.body.appendChild(alert);
+    await alert.present();
+}
+
+submitBtn.addEventListener("click", function() {
+
+    let count = 0
+    let quizAnswers = [answerOne.value, answerTwo.value, answerThree.value, answerFour.value, answerFive.value]
+    const answers = ['3', '4', 'Philippines', 'Facebook', 'Ionic']
+
+    if (answerOne.value === '' || answerTwo.value === '' || answerThree.value === '' || answerFour.value === '' || answerFive.value === '') {
+        presentAlert()
+    } else {
+        for (let i = 0; i < quizAnswers.length; i++) {
+            if (quizAnswers[i] === answers [i])
+                count++
+        }
+    }
+
+    console.log(count)
+})
