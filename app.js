181aaa828f5c8a43339622214fb69526159db9ea
David Genesis Pedeglorio <pedegloriodavidgenesis@gmail.com>
2022-09-27 21:51:51 +0800

    Create card for displaying quiz result

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const answerTwo = document.getElementById('answer-2')
 const answerThree = document.getElementById('answer-3')
 const answerFour = document.getElementById('answer-4')
 const answerFive = document.getElementById('answer-5')
+const resultScore = document.getElementById('result')
 
 async function presentAlert() {
     const alert = document.createElement('ion-alert');
@@ -32,4 +33,14 @@ submitBtn.addEventListener("click", function() {
     }
 
     console.log(count)
+
+    const resultElement = document.createElement('ion-card')
+    resultElement.innerHTML = `    
+    <ion-card-header>
+        <ion-card-title>Quiz Result: ${count}/5</ion-card-title>
+        <ion-card-subtitle>Have a great day!</ion-card-subtitle>
+    </ion-card-header>    
+    `
+    resultScore.appendChild(resultElement)
+
 })
